efe9dab67a8d43fce847b403d72a1a95a5eeef0d
Higiniecito <cdramirezh@unal.edu.co>
2025-10-17 10:16:04 -0500

    12 Saludo personalizado con nombre desde Whatsapp

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -7,12 +7,12 @@ class MessageHandler {
     return greetings.includes(message);
   }
 
-  async handleIncomingMessage(message) {
+  async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
       const incomingMessage = message.text.body.toLowerCase().trim();
 
       if (this.isGreeting(incomingMessage)) {
-        await this.sendWelcomeMessage(message.from, message.id);
+        await this.sendWelcomeMessage(message.from, message.id, senderInfo);
       } else {
         const response = `Echo: ${message.text.body}`;
         await whatsappService.sendMessage(message.from, response, message.id);
@@ -21,9 +21,11 @@ class MessageHandler {
     }
   }
 
-  async sendWelcomeMessage(to, messageId) {
-    const welcomeMessage = "Hola, Bienvenido a nuestro servicio de Veterinaria online. "
-      + "En qué puedo ayudarte hoy?";
+  async sendWelcomeMessage(to, messageId, senderInfo) {
+    const senderFullName = senderInfo?.profile?.name || senderInfo.wa_id;
+    const senderFirstName = senderFullName.split(' ')[0] || 'there';
+    const welcomeMessage = `Hola ${senderFirstName}, Bienvenido a nuestro servicio de Veterinaria online. 
+En qué puedo ayudarte hoy?`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
   }
 }
